81b142deae82ab339ab509b0e6e054668a951bc9
Frédéric Delorme <frederic.delorme@gmail.com>
2021-03-23 23:42:13 +0100

    feat(entities): manage multiple entities

---
+++ modules/entity.js
@@ -0,0 +1,72 @@
+const ENTITY_TYPE = {
+  RECTANGLE: 1,
+  CIRCLE: 2,
+  POINT: 3,
+}
+
+class Entity {
+  constructor(x=0, y=0, w=8, h=8) {
+
+    // position
+    this.x = x;
+    this.y = y;
+    this.angle = 0;
+    this.type = ENTITY_TYPE.RECTANGLE;
+    //size
+    this.w = w;
+    this.h = h;
+    // velocity
+    this.dx = 0;
+    this.dy = 0;
+    this.dr = 0;
+    // drawing priorities
+    this.priority = 1;
+    this.layer = 1;
+    // colors
+    this.color = {
+      r: 255,
+      g: 127,
+      b: 127,
+      a: 1
+    };
+  }
+  setSpeed(dx, dy) {
+    this.dx = dx;
+    this.dy = dy;
+  }
+
+  setRSpeed(dr) {
+    this.dr = dr;
+  }
+
+  setColor(r=255, g=255, b=255, a=1) {
+    this.color.r = r;
+    this.color.g = g;
+    this.color.b = b;
+    this.color.a = a;
+  }
+
+  update() {
+    this.x += this.dx;
+    this.y += this.dy;
+    this.angle += this.dr;
+  }
+
+  draw() {
+    noStroke();
+    fill(this.color.r, this.color.g, this.color.b);
+    translate(this.x + (this.w / 2), this.y + (this.h / 2));
+    rotateZ(this.angle);
+    switch (this.type) {
+    case ENTITY_TYPE.RECTANGLE:
+      rect(0, 0, this.w, this.h);
+      break
+    case ENTITY_TYPE.CIRCLE:
+      circle(0, 0, this.w);
+      break
+    case ENTITY_TYPE.POINT:
+      point(this.x, this.y);
+      break
+    }
+  }
+}
--- modules/sketch.js
+++ modules/sketch.js
@@ -1,30 +1,71 @@
-let x = 0,
-  y = 0;
-let dx = 1,
-  dy = 1;
-let sx = 50,
-  sy = 50;
-let angle = 0;
+var entities = [];
 
 function setup() {
-  createCanvas(640, 360, WEBGL);
+  createCanvas(640, 480, WEBGL);
+  initialize();
+}
+
+function initialize() {
+  for (var i = 0; i < 200; i++) {
+    let entity = new Entity(
+      random(0,width),
+      random(0,height),
+      random(4, 16),random(4, 16));
+    entity.setSpeed(random(-5, 5), random(-5, 5));
+    entity.setRSpeed(random(-0.05, 0.05))
+    entity.setColor(random(0, 255), random(0, 255), random(0, 255))
+    entity.type = ENTITY_TYPE.CIRCLE;
+    entities.push(entity);
+  }
+
 }
 
 function draw() {
-  rectMode(CENTER)
+  rectMode(CENTER);
   translate(-width / 2, -height / 2);
   background(51);
-  fill(244);
-  translate(x, y);
-  rotateZ(angle);
-  rect(0, 0, sx, sy);
-  x += dx;
-  y += dy;
-  angle += 0.07;
-  if ((x > width - sx) | (x < 0)) {
-    dx = -dx;
+  update();
+  constrainToViewPort();
+  render();
+}
+
+function update() {
+  entities.forEach((e)=>{
+    e.update();
   }
-  if ((y > height - sy) | (y < 0)) {
-    dy = -dy;
+  );
+}
+
+function constrainToViewPort() {
+  entities.forEach((e)=>{
+    var w = 0, h = 0;
+    switch(e.type){
+      case ENTITY_TYPE.POINT:
+        w = 1; h = 1;
+        break;
+      case ENTITY_TYPE.CIRCLE:
+        w = e.w; h= e.w;
+        break;
+      case ENTITY_TYPE.RECTANGLE:
+        w = e .w; h = e.h;
+        break;
+    }
+    if ((e.x > width - w) | (e.x < 0)) {
+      e.dx = -e.dx;
+    }
+    if ((e.y > height - h) | (e.y < 0)) {
+      e.dy = -e.dy;
+    }
+
   }
+  )
+}
+
+function render() {
+  entities.forEach((e)=>{
+    push();
+    e.draw();
+    pop();
+  }
+  );
 }
